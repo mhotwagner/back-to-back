ce04e3de587c187328dea08e9e53789fa3164e71
Michael Hotwagner <mhotwagner@gmail.com>
2018-02-17 22:23:16 -0500

    Add number property to Calculation
    Use factory instead of instantiating models in test.

--- back_to_back/apps/calculator/models.py
+++ back_to_back/apps/calculator/models.py
@@ -4,3 +4,7 @@ from django.db import models
 # Create your models here.
 class Calculation(models.Model):
     id = models.IntegerField(null=False, primary_key=True)
+
+    @property
+    def number(self):
+        return self.id
--- back_to_back/apps/calculator/tests.py
+++ back_to_back/apps/calculator/tests.py
@@ -1,3 +1,6 @@
+
+import factory
+
 from django.db import IntegrityError
 from django.test import TestCase
 
@@ -5,12 +8,21 @@ from django.test import TestCase
 from .models import Calculation
 
 
+class CalculationFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Calculation
+
+
 class TestCalculation(TestCase):
     def test_id_is_a_required_param(self):
         with(self.assertRaises(IntegrityError)):
-            Calculation.objects.create()
+            CalculationFactory.create()
 
     def test_it_can_be_created_with_an_id(self):
-        Calculation.objects.create(id=1)
+        CalculationFactory.create(id=1)
         calculation = Calculation.objects.get(id=1)
         self.assertEqual(calculation.id, 1)
+
+    def test_number_is_same_as_id(self):
+        calculation = CalculationFactory.create(id=1)
+        self.assertEqual(calculation.number, calculation.id)
