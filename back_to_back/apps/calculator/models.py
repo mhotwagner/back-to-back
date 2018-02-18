a4c72e58f013aa768fea2b29bbf6dc04f38924c3
Michael Hotwagner <mhotwagner@gmail.com>
2018-02-17 21:18:35 -0500

    Add simple Calculation model

--- back_to_back/apps/calculator/models.py
+++ back_to_back/apps/calculator/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
+class Calculation(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
--- back_to_back/apps/calculator/tests.py
+++ back_to_back/apps/calculator/tests.py
@@ -1,3 +1,16 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Calculation
+
+
+class TestCalculation(TestCase):
+    def test_id_is_a_required_param(self):
+        with(self.assertRaises(IntegrityError)):
+            Calculation.objects.create()
+
+    def test_it_can_be_created_with_an_id(self):
+        Calculation.objects.create(id=1)
+        calculation = Calculation.objects.get(id=1)
+        self.assertEqual(calculation.id, 1)
